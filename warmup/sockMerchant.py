91fc853bdca741a262236b314c022b2823d9e394
amreyesp <amreyesp@unal.edu.co>
2019-05-26 18:22:02 -0500

    implement test and function to identify colours and count the socks of each colour

--- warmup/sockMerchant.py
+++ warmup/sockMerchant.py
@@ -15,9 +15,20 @@ class Sockbag:
 
     def __init__(self):
         self.bag = []
+        self.colours = []
+        self.socks = []
+
+    def count_socks(self):
+        for colour in self.colours:
+            self.socks.append(self.bag.count(colour))
+
+    def id_colours(self):
+        for sock in self.bag:
+            if sock not in self.colours:
+                self.colours.append(sock)
 
     def fill(self,size,colour):
-        for i in range(size):
+        for sock in range(size):
             self.bag.append(random.randint(1,colour))
 
     def sort_bag(self):
