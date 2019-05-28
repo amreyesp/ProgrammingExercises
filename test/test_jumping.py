971e0914e677ea70820183374bb426c82e94e5a5
amreyesp <amreyesp@unal.edu.co>
2019-05-28 18:53:39 -0500

    implement adjustClouding fuction and test to ensure that there are not two thunderheads in a row

--- test/test_jumping.py
+++ test/test_jumping.py
@@ -16,11 +16,23 @@ class TestJumpingClouds(unittest.TestCase):
         self.game.clouding(10)
         self.assertEqual(10,len(self.game.clouds))
 
+    #Clouds values can only be binary
     def test_valueClouds(self):
         self.game.clouding(10)
         value = True
         for cloud in self.game.clouds:
             if cloud != 0 and cloud != 1:
+                value = False
+                break
+        self.assertTrue(value)
+
+    #The set of clouds must be compatible with the jump steps rules (1 or 2 steps)
+    def test_proximityThunderheads(self):
+        self.game.clouding(100)
+        value = True
+        self.game.adjustClouding()
+        for i in range(len(self.game.clouds)-2):
+            if self.game.clouds[i+1] == 1 and self.game.clouds[i+2] == 1:
                 value = False
                 break
         self.assertTrue(value)
