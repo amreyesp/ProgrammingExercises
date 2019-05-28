6f6e64525f610d6dc76e63cde00fb7a48e1429be
amreyesp <amreyesp@unal.edu.co>
2019-05-28 16:43:46 -0500

    implement setUp() method

--- test/test_socket.py
+++ test/test_socket.py
@@ -9,42 +9,36 @@ from warmup.sockMerchant import Sockbag
 
 class TestSockMerchant(unittest.TestCase):
 
-    def set_up(self):
+    def setUp(self):
         self.sockbag = Sockbag()
 
     def bag_example(self):
         self.sockbag.bag = [2,3,4,1,2,5,6,7,4,1]
 
     def test_initial_value(self):
-        self.set_up()
         self.assertEqual([], self.sockbag.bag)
 
     def test_fill_bag(self):
-        self.set_up()
         self.sockbag.fill(5,2)
         self.assertEqual(5, len(self.sockbag.bag))
 
     def test_sort_bag(self):
-        self.set_up()
         self.sockbag.bag = [2,3,4,1,2,5,6,7,4,1]
         self.sockbag.sort_bag()
         self.assertEqual([1,1,2,2,3,4,4,5,6,7],self.sockbag.bag)
 
     def test_id_colours(self):
-        self.set_up()
         self.sockbag.bag = [1,1,2,2,3,4,4,5,6,7]
         self.sockbag.id_colours()
         self.assertEqual([1,2,3,4,5,6,7],self.sockbag.colours)
 
     def test_count_socks(self):
-        self.set_up()
         self.sockbag.bag = [1,1,2,2,3,4,4,5,6,7]
         self.sockbag.colours = [1,2,3,4,5,6,7]
         self.sockbag.count_socks()
         self.assertEqual([2,2,1,2,1,1,1],self.sockbag.socks)
 
     def test_count_pairs(self):
-        self.set_up()
         self.sockbag.colours = [1,2,3,4,5,6,7]
         self.sockbag.socks = [2,2,1,2,1,1,1]
         self.sockbag.count_pairs()
