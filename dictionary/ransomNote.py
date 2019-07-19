a32b36aca9b4f87ea0a38c044ab8f2b540be8900
amreyesp <amreyesp@unal.edu.co>
2019-07-18 21:43:32 -0500

    implement test and function for ransom note exercise solution

--- dictionary/ransomNote.py
+++ dictionary/ransomNote.py
@@ -11,8 +11,36 @@ Given the words in the magazine and the words in the ransom note, print Yes if
 he can replicate his ransom note exactly using whole words from the magazine;
 otherwise, print No.
 
-For example, the note is "Attack at dawn". The magazine contains only 
+For example, the note is "Attack at dawn". The magazine contains only
 "attack at dawn". The magazine has all the right words, but there's a case
 mismatch. The answer is No.
 Credits: www.hackerrank.com
 """
+
+class NewNote:
+
+    def __init__(self):
+        self.magazine_size = 0
+        self.note_size = 0
+        self.magazine = []
+        self.note = []
+
+    def read_input(self,size_mag,size_note,mag_words,note_words):
+        self.magazine_size = size_mag
+        self.note_size = size_note
+
+        words = mag_words.split()
+        for word in words:
+            self.magazine.append(word)
+
+        words = note_words.split()
+        for word in words:
+            self.note.append(word)
+
+    def check_magazine(self):
+        for word_note in self.note:
+            if word_note in self.magazine:
+                self.magazine.remove(word_note)
+            else:
+                return 'No'
+        return 'Yes'
--- test/test_ransomnote.py
+++ test/test_ransomnote.py
@@ -4,3 +4,36 @@ import unittest
 import os, sys
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+
+from dictionary.ransomNote import NewNote
+
+class TestRansomNote(unittest.TestCase):
+
+    def setUp(self):
+        self.my_note = NewNote()
+        self.assertEqual(0,self.my_note.magazine_size)
+        self.assertEqual(0,self.my_note.note_size)
+        self.assertEqual([],self.my_note.magazine)
+        self.assertEqual([],self.my_note.note)
+
+    def test_read_input(self):
+        self.my_note.read_input(6,4,'give me one grand today night','give one grand today')
+        self.assertEqual(6,self.my_note.magazine_size)
+        self.assertEqual(4,self.my_note.note_size)
+        self.assertEqual(['give','me','one','grand','today','night'],self.my_note.magazine)
+        self.assertEqual(['give','one','grand','today'],self.my_note.note)
+
+    def test_check_magazine(self):
+        self.my_note.read_input(6,4,'give me one grand today night','give one grand today')
+        ans = self.my_note.check_magazine()
+        self.assertEqual('Yes',ans)
+
+    def test_input1(self):
+        self.my_note.read_input(6,5,'two times three is not four','two times two is four')
+        ans = self.my_note.check_magazine()
+        self.assertEqual('No',ans)
+
+    def test_input2(self):
+        self.my_note.read_input(7,4,'ive got a lovely bunch of coconuts','ive got some coconuts')
+        ans = self.my_note.check_magazine()
+        self.assertEqual('No',ans)
