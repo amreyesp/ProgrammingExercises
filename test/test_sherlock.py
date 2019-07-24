e526668c8164c66b9105a541a0e7d739f0a20edd
amreyesp <amreyesp@unal.edu.co>
2019-07-23 19:37:52 -0500

    implement test and method for id anagrams on sherlock exercise

--- test/test_sherlock.py
+++ test/test_sherlock.py
@@ -12,18 +12,23 @@ class TestSherlock(unittest.TestCase):
     def setUp(self):
         self.my_anagram = NewAnagram()
         self.assertEqual('',self.my_anagram.string)
-        self.assertEqual({},self.my_anagram.pairs)
+        self.assertEqual({},self.my_anagram.anagrams)
 
     def test_read_input(self):
         self.my_anagram.read_input('mom')
         self.assertEqual('mom',self.my_anagram.string)
 
-    def test_id_anagrams_length1(self):
+    def test_anagrams_length1(self):
         self.my_anagram.read_input('mom')
-        self.my_anagram.id_anagrams(length=1)
-        self.assertEqual({'m':[[0],[2]],'o':[[1]]},self.my_anagram.pairs)
+        self.my_anagram.anagrams_one_letter(length=1)
+        self.assertEqual({'m':[[0],[2]],'o':[[1]]},self.my_anagram.anagrams)
+
+    def test_id_anagrams_length2(self):
+        self.my_anagram.read_input('mom')
+        self.my_anagram.id_anagrams(length=2)
+        self.assertEqual({'mo':[[0,1]],'mm':[[0,2]],'om':[[1,2]]},self.my_anagram.anagrams)
 
     # def test_anagrams_length1(self):
     #     self.my_anagram.read_input('momo')
-    #     pairs = self.my_anagram.anagrams(length=1)
-    #     self.assertEqual(2,self.my_anagram.pairs)
+    #     anagrams = self.my_anagram.anagrams(length=1)
+    #     self.assertEqual(2,self.my_anagram.anagrams)
