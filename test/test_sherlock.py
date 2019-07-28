5a6cd37ef102c2aa6bcaf4b15d1f9dcf476c7ba9
amreyesp <amreyesp@unal.edu.co>
2019-07-28 08:06:30 -0500

    implement test and method to find all anagrams in a string

--- test/test_sherlock.py
+++ test/test_sherlock.py
@@ -36,8 +36,20 @@ class TestSherlock(unittest.TestCase):
     def test_find_anagrams_two_letters(self):
         self.my_anagram.read_input('mom')
         self.my_anagram.all_substrings()
-        ans = self.my_anagram.find_anagrams('mo')
+        ans = self.my_anagram.find_anagram('mo')
         self.assertEqual({'m':[[0],[2]],'o':[[1]],'mo':[[0, 1],[1, 2]]},ans)
+
+    def test_find_all_anagrams(self):
+        self.my_anagram.read_input('mom')
+        self.my_anagram.all_substrings()
+        self.my_anagram.find_all_anagrams()
+        self.assertEqual({'m':[[0],[2]],'mo':[[0, 1],[1, 2]]},self.my_anagram.anagrams)
+
+    def test_find_all_anagrams_input2(self):
+        self.my_anagram.read_input('abba')
+        self.my_anagram.all_substrings()
+        self.my_anagram.find_all_anagrams()
+        self.assertEqual({'a':[[0],[3]],'b':[[1],[2]],'ab':[[0,1],[2,3]],'abb':[[0,1,2],[1,2,3]]},self.my_anagram.anagrams)
 
     # def test_count_pairs(self):
     #     self.my_anagram.read_input('mom')
