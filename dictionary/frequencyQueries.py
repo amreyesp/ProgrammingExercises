60efb89ea22cb15ea6ffe32abd76ba61c338c3f3
amreyesp <amreyesp@unal.edu.co>
2019-08-02 21:50:43 -0500

    create files for test and frequency queries exercise implementation

---
+++ dictionary/frequencyQueries.py
@@ -0,0 +1,13 @@
+"""Frequency Queries"""
+"""
+You are given q queries. Each query is of the form two integers described below:
+- 1x : Insert x in your data structure.
+- 2y : Delete one occurence of y from your data structure, if present.
+- 3z : Check if any integer is present whose frequency is exactly z. If yes,
+print 1 else 0.
+
+The queries are given in the form of a 2-D array queries of q size  where q[i][0]
+contains the operation, and queries[i][1] contains the data element. For example,
+you are given array queries = [(1,1),(2,2),(3,2),(1,1),(1,1),(2,1),(3,2)].
+Credits: www.hackerrank.com
+"""
