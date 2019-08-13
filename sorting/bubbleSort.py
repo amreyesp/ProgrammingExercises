d0cb90e039e68952732285f92fe48238e81ab45b
amreyesp <amreyesp@unal.edu.co>
2019-08-13 15:26:10 -0500

    create files for bubble sort exercise

---
+++ sorting/bubbleSort.py
@@ -0,0 +1,9 @@
+"""Sorting: Bubble Sort"""
+"""
+Considering the Bubble sort algorithm, given an array of integers, sort the array
+in ascending order using it. Once sorted, print the following three lines:
+1.Array is sorted in numSwaps swaps., where  numSwaps is the number of swaps that took place.
+2.First Element: firstElement, where firstElement is the first element in the sorted array.
+3.Last Element: lastElement, where lastElement is the last element in the sorted array.
+Credits: www.hackerrank.com
+"""
